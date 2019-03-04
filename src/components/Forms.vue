779f21a2e84d1eb98ba31a7fb00f858841c50949
Ivan Tkachenko <vanekt@yandex.ru>
2019-03-05 02:39:21 +0300

    simple form inputs

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    forms
+    <h2>Form inputs</h2>
+    <hr>
+
+    <input type="text" v-model.lazy="textInputValue" />
+    <p>{{textInputValue}}</p>
+    <hr>
+
+    <textarea v-model="textareaValue" rows="5"></textarea>
+    <div class="textarea-value">{{textareaValue}}</div>
+    <hr>
+
+    <label><input type="checkbox" value="fb" v-model="socialCheckbox"> Facebook</label>
+    <label><input type="checkbox" value="vk" v-model="socialCheckbox"> Vkontakte</label>
+    <label><input type="checkbox" value="instagram" v-model="socialCheckbox"> Instagram</label>
+    <ul>
+      <li v-for="item in socialCheckbox" :key="item">{{item}}</li>
+    </ul>
+    <hr>
+
+    <label><input type="radio" value="fb" v-model="socialRadio"> Facebook</label>
+    <label><input type="radio" value="vk" v-model="socialRadio"> Vkontakte</label>
+    <label><input type="radio" value="instagram" v-model="socialRadio"> Instagram</label>
+    <p>{{socialRadio}}</p>
+    <hr>
+
+    <select v-model="socialSelect.selected">
+      <option v-for="item in socialSelect.items" :value="item.value">{{item.label}}</option>
+    </select>
   </div>
 </template>
 
@@ -8,10 +35,26 @@
 
 export default {
   name: 'Forms',
+  data: () => ({
+    textInputValue: '',
+    textareaValue: '',
+    socialCheckbox: ['fb'],
+    socialRadio: 'vk',
+    socialSelect: {
+      selected: 'vk',
+      items: [
+        { label: 'Facebook', value: 'fb' },
+        { label: 'Vkontakte', value: 'vk' },
+        { label: 'Instagram', value: 'instagram' },
+      ],
+    },
+  }),
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .textarea-value {
+    white-space: pre;
+  }
 </style>
